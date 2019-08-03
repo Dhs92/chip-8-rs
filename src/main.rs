4ea4f124d32e20d81d8b60957afed1061ddd77d1
Alexander Forehand <forehand.alex@gmail.com>
2019-08-03 13:11:25 -0700

    Finished pattern match for opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 mod cpu;
 use crate::cpu::*;
-use std::io;
+use itertools::Itertools;
 use std::fmt;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 
 #[derive(Debug)]
 enum Error {
@@ -14,6 +16,11 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self)
     }
+}
+
+struct Cartridge {
+    pub rom: [u8; 3584],
+    pub size: usize,
 }
 
 static mut CPU: Cpu = Cpu {
@@ -29,113 +36,187 @@ static mut CPU: Cpu = Cpu {
 };
 
 fn main() -> io::Result<()> {
-    let mut opcode = String::new();
+    let mut file = fs::File::open("15_Puzzle_[Roger Ivie].ch8")?;
+    let mut memory: [u8; 3584] = [0; 3584];
+    let bytes_read = file.read(&mut memory)?;
 
-    println!("Reading to string...");
-    io::stdin().read_line(&mut opcode)?;
+    for chunk in &memory.iter().chunks(2) {
+        let mut opcode = 0;
+        let mut b1: u16 = 0;
+        let mut b2: u16 = 0;
+        for (i, b) in chunk.enumerate() {
+            if i == 1 {
+                b1 = *b as u16
+            } else {
+                b2 = *b as u16
+            }
+        }
+        opcode = b1 << 8 | b2;
+        decode_opcode(opcode).unwrap()
+    }
 
-    println!("Input: {}", opcode);
-
-    let opcode: String = opcode.trim_end().to_string();
-
-    println!("Post trim: {}", opcode);
-
-    let decoded: Vec<u8> = hex::decode(opcode).expect("Decoding failed");
-    println!("{:X?}", decoded);
-
-    let opcode: u16 = (decoded[0] as u16) << 8 | (decoded[1] as u16);
-    //assert_eq!(opcode, 0xA123);
-    decode_opcode(opcode).unwrap();
+    //println!("{:4X?}", memory);
 
     Ok(())
 }
 
+fn get_opcode(b1: u8, b2: u8) -> u16 {
+    (b1 as u16) << 8 | (b2 as u16)
+}
+
 fn decode_opcode(opcode: u16) -> Result<(), Error> {
     match opcode & 0xF000 {
-        0xA000 => {
-            println!("Opcode: [{:>04X}]", opcode);
-            Ok(())
-        }
         0x0000 => match opcode & 0x00FF {
             0x00E0 => {
-                println!("E1: [{:>4X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x00EE => {
-                println!("E2: [{:>4X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             _ => {
-                println!("E3: [{:>4X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
         },
         0x1000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x2000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x3000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x4000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x5000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x6000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x7000 => {
-            println!("Opcode: [{:>04X}]", opcode);
+            print!("Opcode: [{:>04X}] ", opcode);
             Ok(())
         }
         0x8000 => match opcode & 0x000F {
             0x0001 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0002 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0003 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0004 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0005 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0006 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x0007 => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             0x000E => {
-                println!("Opcode: [{:>04X}]", opcode);
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            _ => Ok(()),
+        },
+        0x9000 => {
+            print!("Opcode: [{:>04X}] ", opcode);
+            Ok(())
+        }
+        0xA000 => {
+            print!("Opcode: [{:>04X}] ", opcode);
+            Ok(())
+        }
+        0xB000 => {
+            print!("Opcode: [{:>04X}] ", opcode);
+            Ok(())
+        }
+        0xC000 => {
+            print!("Opcode: [{:>04X}] ", opcode);
+            Ok(())
+        }
+        0xD000 => {
+            print!("Opcode: [{:>04X}] ", opcode);
+            Ok(())
+        }
+        0xE000 => match opcode & 0x00FF {
+            0x009E => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x00A1 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            _ => Ok(()),
+        },
+        0xF000 => match opcode & 0x00FF {
+            0x0007 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x000A => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0015 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0018 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x001E => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0029 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0033 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0055 => {
+                print!("Opcode: [{:>04X}] ", opcode);
+                Ok(())
+            }
+            0x0065 => {
+                print!("Opcode: [{:>04X}] ", opcode);
                 Ok(())
             }
             _ => Ok(()),
         },
         _ => {
-            println!("Invalid Opcode: [{:04X}]", opcode);
-            Err(Error::InvalidOpCode())
+            print!("Invalid Opcode: [{:04X}] ", opcode);
+            Ok(())
+            //Err(Error::InvalidOpCode())
         }
     }
 }
